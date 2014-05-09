80ae54c8e4922e82e8c60dde69ad578b0bde8449
Vincent <vincent.elcrin@gmail.com>
2014-05-09 15:16:48 +0200

    Actually no need to replace templateUrl into template. Angular is that good.

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,11 @@ var gulp = require('gulp'),
     minifyHtml = require("gulp-minify-html"),
     concat = require("gulp-concat"),
     uglify = require("gulp-uglify"),
-    replace = require("gulp-replace"),
-    es = require('event-stream'),
     rename = require("gulp-rename"),
-    clean = require("gulp-clean");
 
 gulp.task('build', function () {
 
-    var jsify = gulp.src("src/*.html")
+    gulp.src("src/*.html")
         .pipe(minifyHtml({
             empty: true,
             spare: true,
@@ -18,12 +15,7 @@ gulp.task('build', function () {
         }))
         .pipe(ngHtml2Js({
             moduleName: "directiveSeed"
-        }));
-
-    var wireUp = gulp.src(['src/directive-seed.js'])
-        .pipe(replace(/templateUrl/g, 'template'));
-
-    es.merge(jsify, wireUp)
+        }))
         .pipe(concat("directiveSeed.js"))
         .pipe(gulp.dest('dist'))
         .pipe(rename('directiveSeed.min.js'))
